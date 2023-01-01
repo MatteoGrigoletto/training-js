968172b6659c6ea7b06a3eebc5f9f6a642ab70f9
MatteoGrigoletto <mattgrigoletto@gmail.com>
2023-01-01 03:22:35 +0100

    aggiunge ex

--- fundamentals-2/main.js
+++ fundamentals-2/main.js
@@ -71,4 +71,25 @@ let percentageOfWorld3 = (population) => (population / 8000) * 100;
 
 console.log(percentageOfWorld3(populationsItaly));
 
+
+----------ex- --------
+
+1. Create a function called 'describePopulation'. Use the function type you 
+like the most. This function takes in two arguments: 'country' and 
+'population', and returns a string like this: 'China has 1441 million people, 
+which is about 18.2% of the world.'
+2. To calculate the percentage, 'describePopulation' call the 
+'percentageOfWorld1' you created earlier
+3. Call 'describePopulation' with data for 3 countries of your choice
+
 */
+let country = "italy";
+let population = 60;
+let percentageOfWorld3 = (population) => (population / 8000) * 100;
+
+function describePopulation(country, population) {
+  let percent = percentageOfWorld3(population);
+  let response = `${country} has ${population} million people, which is about ${percent} of the word`;
+  return response;
+}
+console.log(describePopulation(country, population));
