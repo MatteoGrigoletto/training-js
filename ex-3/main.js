03349280c4cd84aa6a2826b1e397f12ebf3bf41a
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-08 13:27:35 +0100

    aggiunge exs 3

--- ex-3/main.js
+++ ex-3/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// ------- ex -------
+let arr = [6, 3, -1, -10, 10];
+let arr1 = [54, 2, 4];
+
+// let temp = function (arr, arr1) {
+//   let concat = arr.concat(arr1);
+//   let max = 0;
+//   let min = 0;
+//   for (let i = 0; i < concat.length; i++) {
+//     let date = concat[i];
+//     if (date > max) {
+//       max = date;
+//     }
+//     if (date < min) {
+//       min = date;
+//     }
+//   }
+//   return max - min;
+// };
+// temp(arr);
+// console.log(temp(arr, arr1));
+
+// ------ ex ---------
+let str = function (arr) {
+  let stringTemps = ``;
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i];
+    stringTemps += `${temp} C in ${i + 1} days, `;
+  }
+  return stringTemps;
+};
+console.log(str(arr));
