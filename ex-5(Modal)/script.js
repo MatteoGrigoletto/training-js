331e088caabd46c91700cae5397b1a3a1151cf34
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-10 19:15:00 +0100

    Aggiunge testi differneti per ogni bottone

--- ex-5(Modal)/script.js
+++ ex-5(Modal)/script.js
@@ -4,11 +4,28 @@ let btnModal = document.querySelectorAll('.show-modal');
 let modal = document.querySelector('.modal');
 let btnCloseModal = document.querySelector('.close-modal');
 let overlay = document.querySelector('.overlay');
+let first = document.querySelector('.first');
+let second = document.querySelector('.second');
+let third = document.querySelector('.third');
 
 for (let i = 0; i < btnModal.length; i++) {
   btnModal[i].addEventListener('click', function () {
+    first.classList.remove('hidden');
+    second.classList.remove('hidden');
+    third.classList.remove('hidden');
+
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
+    if (btnModal[i].innerHTML == `Show modal 1`) {
+      second.classList.add('hidden');
+      third.classList.add('hidden');
+    } else if (btnModal[i].innerHTML == `Show modal 2`) {
+      first.classList.add('hidden');
+      third.classList.add('hidden');
+    } else if (btnModal[i].innerHTML == `Show modal 3`) {
+      first.classList.add('hidden');
+      second.classList.add('hidden');
+    }
   });
   btnCloseModal.addEventListener('click', function () {
     modal.classList.add('hidden');
