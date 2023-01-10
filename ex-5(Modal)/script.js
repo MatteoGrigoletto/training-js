b95d379dae0fdf9b4768cf3abf220323635c9db8
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-10 18:46:16 +0100

    Aggiunge modale

--- ex-5(Modal)/script.js
+++ ex-5(Modal)/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let btnModal = document.querySelectorAll('.show-modal');
+let modal = document.querySelector('.modal');
+let btnCloseModal = document.querySelector('.close-modal');
+let overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < btnModal.length; i++) {
+  btnModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+  overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  });
+}
