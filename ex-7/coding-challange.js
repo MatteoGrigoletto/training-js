ea45b180448aec8ce81c3b810e6b3ad288c01e2c
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-21 14:11:02 +0100

    aggiunge risposta problema

--- ex-7/coding-challange.js
+++ ex-7/coding-challange.js
@@ -125,77 +125,114 @@
 //  Lewandowski: 2
 // }
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-  printGoals: function (...names) {
-    let totalPlayer = '';
-    let totalScore = 0;
-    names.forEach(player => {
-      if (this.scored.includes(player)) {
-        totalScore++;
-      }
-      totalPlayer += `${player},`;
-    });
-    return totalPlayer + `total score ${totalScore}`;
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+//   printGoals: function (...names) {
+//     let totalPlayer = '';
+//     let totalScore = 0;
+//     names.forEach(player => {
+//       if (this.scored.includes(player)) {
+//         totalScore++;
+//       }
+//       totalPlayer += `${player},`;
+//     });
+//     return totalPlayer + `total score ${totalScore}`;
+//   },
+// };
 
-for ([index, player] of game.scored.entries()) {
-  console.log(`Goal ${index + 1}: ${player}`);
-}
+// for ([index, player] of game.scored.entries()) {
+//   console.log(`Goal ${index + 1}: ${player}`);
+// }
 
-const odds = Object.values(game.odds);
-let average = 0;
-for (const odd of odds) average += odd;
-average /= odds.length;
-console.log(average);
+// const odds = Object.values(game.odds);
+// let average = 0;
+// for (const odd of odds) average += odd;
+// average /= odds.length;
+// console.log(average);
 
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
-  console.log(`Odd of ${teamStr} ${odd}`);
-}
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamStr} ${odd}`);
+// }
 
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-  console.log(scorers);
-}
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+//   console.log(scorers);
+// }
 
-console.log(`=======================`);
+// console.log(`=======================`);
+
+// - Allora, abbiamo una funzione che accetta una stringa come argomento.
+
+// - La stringa e' in formato HTML e contiene  un numero indefinito di <div> che non sono chiusi correttamente.
+// (esempio: <div> <p> Ciao a tutti </p> <div> ).
+// All'interno dell'esempio manca la chiusura del secondo DIV cioe' la '/'  .
+
+// La funzione deve prendere la stringa sbagliata e restituirla nella versione corretta,
+// in questo caso:
+//                Versione sbagliata: <div> <p> Ciao a tutti </p> <div>
+//                Versione corretta:   <div> <p> Ciao a tutti </p> </div>
+
+// altro esempio:   <div><div> <div><div>   ----->  <div><div> </div></div>
+
+// const count = string => {
+//   let divCounter = 0;
+//   let unknowFour = '';
+//   let fixedHTML = '';
+
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === '<') {
+//       for (let j = 1; j < 5; j++) {
+//         unknowFour += string[i + j];
+//       }
+//     }
+//     if (unknowFour === 'div>') {
+//       divCounter++;
+//       if (divCounter % 2 === 0) {
+//         fixedHTML += string[i] + '/';
+//       }
+//     } else {
+//       fixedHTML += string[i];
+//     }
+//     unknowFour = '';
+//   }
+//   return fixedHTML;
+// };
