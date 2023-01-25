9ea91c4fe8e767afd44ae47a19b23c629f739bcf
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-25 13:51:34 +0100

    aggiunge spunti

--- ex-8(callback_function)/script.js
+++ ex-8(callback_function)/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// const first = function (saluto) {
+//   return function second(nome) {
+//     console.log(` ${saluto} ${nome}`);
+//   };
+// };
+
+// const final = first('ciao');
+
+// final('Matteo');
+
+// first('ciao')('matteo');
+
+// const double = nome => cognome => `ciao mi chiamo ${nome} ${cognome}`;
+// console.log(double('matteo')('grigoletto'));
+
+// // const fullname = persona.fullname
+
+// // fullname.call()
+let arr = [1, 2, 3];
+// let ob = new Map(Object.entries(arr));
+// // let [key, value] = ob[1];
+// // console.log(ob);
+// for (let [key, value] of ob) {
+//   console.log(key);
+// }
+
+// const [a, ...b] = arr;
+// console.log(b);
+// let newnumber = [...arr, 5, 4];
+
+// console.log(newnumber);
+
+let uno = {
+  nome: `matteo`,
+  cognome: `grigo`,
+};
+
+console.log(Object.keys(arr));
