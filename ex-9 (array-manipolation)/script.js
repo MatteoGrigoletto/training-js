a0b3c45f934bd3931ff820d5fb7edd356b61c700
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-02-05 21:15:43 +0100

    metodo some and every

--- ex-9 (array-manipolation)/script.js
+++ ex-9 (array-manipolation)/script.js
@@ -1,1 +1,10 @@
 'use strict';
+// metodo some e every iterano l'intero array e controllano se la condizione e' vera o falsa
+// some al primo riscontro positivo ritorna true
+// every invece solamente se tutti gli elementi soddisfano la condizione
+
+// let arr = [34, 543, 45556, 756, 536367, -434, -43423];
+
+// let bool = arr.some(elm => elm > 0);
+// let bool1 = arr.every(elm => elm > 0 || elm < 0);
+// console.log(bool, bool1);
